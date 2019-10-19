4714d373cb81463c23c9931d19fbefefc52a3f3d
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-10-19 08:39:18 +0300

    added sum calculation

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ let isNumber = (element) => {
     }
   }
 }
+
+let countSum = (arr) => {
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (isNumber(arr[i])) {
+      sum = sum + arr[i]
+    }
+  }
+  return sum
+}
